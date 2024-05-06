49e5951e672fc2e93a012e15f16d57d92ba294df
akshay <akshaycyber96@gmail.com>
2024-05-06 18:18:59 +0530

    updated

--- api/helpers/logger/index.js
+++ api/helpers/logger/index.js
@@ -5,17 +5,17 @@ const todayDate = new Date().toISOString().slice(0, 10);
 const logger = createLogger({
     transports : [
         new transports.File({
-            filename : 'nodejs-logs/info/info-log-' + todayDate + '.log',
+            filename : 'logs/info/info-log-' + todayDate + '.log',
             level : 'info',
             format : format.combine(format.timestamp() , format.json())
         }),
         new transports.File({
-            filename : 'nodejs-logs/error/error-log-' + todayDate + '.log',
+            filename : 'logs/error/error-log-' + todayDate + '.log',
             level : 'error',
             format : format.combine(format.timestamp() , format.json())
         }),
         new transports.File({
-            filename : 'nodejs-logs/warn/warn-log-' + todayDate + '.log',
+            filename : 'logs/warn/warn-log-' + todayDate + '.log',
             level : 'warn',
             format : format.combine(format.timestamp() , format.json())
         })
