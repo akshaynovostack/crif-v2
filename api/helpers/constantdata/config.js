374759bd7ece646534ce4872dd2a7e99011c8152
akshay <akshaycyber96@gmail.com>
2024-05-13 11:41:54 +0530

    updated

--- api/helpers/constantdata/config.js
+++ api/helpers/constantdata/config.js
@@ -21,9 +21,6 @@ exports.customShortnerBasePath = process.env.CUSTOMER_SHORTNER_BASE_PATH;
 exports.customShortnerApiKey = process.env.CUSTOMER_SHORTNER_API_KEY;
 
 
-exports.falseSightApiKey = process.env.FALSE_SIGHT_API_KEY ? process.env.FALSE_SIGHT_API_KEY : "kjbasdbakdb";
-exports.falseSightApiUrl = process.env.FALSE_SIGHT_BASE_URL ? process.env.FALSE_SIGHT_BASE_URL : "https://falsesight.com/api/";
-
 exports.environment = environment;
 
 exports.activeBureauPartner = process.env.ACTIVE_BUREAU_PARTNER ? process.env.ACTIVE_BUREAU_PARTNER : "crif";
--- api/helpers/constantdata/dbSetting.js
+++
@@ -1,9 +0,0 @@
-exports.getSettingValue = async (settingName) => {
-    try{
-        let settingValue = await knexSqlDb.from("settings").where({setting_name:settingName});
-        return {settingValue};
-    }
-    catch(e){
-        return {error:e};
-    }
-}
